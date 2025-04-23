1e3dcf2897a3535aaa1e9e9402484e1d83cf39b4
HeliR033 <24BIT033@sot.pdpu.ac.in>
2025-04-23 18:29:20 +0530

    Add files via upload

---
+++ Lab 6/L6q2.py
@@ -0,0 +1,10 @@
+l=[(1, 'heli', 19), (2, 'aastha', 18), (3, 'mansi', 18)]
+print(l)
+l1=[]
+l2=[]
+l3=[]
+for i in l:
+    l1.append(i[0])
+    l2.append(i[1])
+    l3.append(i[2])
+print(l1,l2,l3,sep='\n')
